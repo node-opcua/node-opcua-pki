bfc1ebaa68e4d555088c01dd374695eaca5fbdae
Bjørn Bråthen <bjorbrat88@gmail.com>
2018-01-22 14:31:21 +0100

    Less code duplication.

--- test/test_certificate_manager.js
+++ test/test_certificate_manager.js
@@ -149,9 +149,7 @@ describe("CertificateManager managing certificate", function () {
     function createSampleCertificateDer(certificate,callback) {
 
         var default_openssl_conf_path = path.join(__dirname,"../tmp/PKI2/own/openssl.cnf");
-        var default_openssl_conf = q(n(default_openssl_conf_path));
-        var default_openssl_conf_output = default_openssl_conf_path + '.tmp';
-        toolbox.generateStaticConfig(default_openssl_conf_path, default_openssl_conf_output);
+        var default_openssl_conf = toolbox.generateStaticConfig(default_openssl_conf_path);
         assert(_.isFunction(callback));
 
         certificate = toolbox.make_path(certificate);
@@ -161,7 +159,7 @@ describe("CertificateManager managing certificate", function () {
             "-x509 -days 365 -nodes -newkey rsa:1024 " +
             "-batch -keyout private_key.pem " +
             "-outform der -out " + q(n(certificate)) +
-            " -config " + default_openssl_conf_output,{},function(err){
+            " -config " + q(n(default_openssl_conf)),{},function(err){
             assert(fs.existsSync(certificate));
 
             callback(err);
