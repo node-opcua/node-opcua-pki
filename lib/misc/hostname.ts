b881334b2393a7cfd2fed5f1e5bd0f7ff0409e51
Etienne Rossignon <etienne.rossignon@gadz.org>
2020-12-20 08:40:19 +0100

    update hostname

--- lib/misc/hostname.ts
+++ lib/misc/hostname.ts
@@ -1,3 +1,6 @@
+/**
+ * @module node-opcua-hostname
+ */
 import * as dns from "dns";
 import * as os from "os";
 import { promisify } from "util";
@@ -29,18 +32,24 @@ function fqdn(callback: (err: Error | null, fqdn?: string) => void) {
 
 let _fullyQualifiedDomainNameInCache: string | undefined;
 
+/**
+ * extract FullyQualifiedDomainName of this computer
+ */
 export async function extractFullyQualifiedDomainName(): Promise<string> {
     if (_fullyQualifiedDomainNameInCache) {
         return _fullyQualifiedDomainNameInCache;
     }
-    if (false && process.platform === "win32") {
+    if (process.platform === "win32") {
         // http://serverfault.com/a/73643/251863
-        const env = process.env;
+        const env = process.env;http://serverfault.com/a/73643/251863
         _fullyQualifiedDomainNameInCache =
             env.COMPUTERNAME + (env.USERDNSDOMAIN && env.USERDNSDOMAIN!.length > 0 ? "." + env.USERDNSDOMAIN! : "");
     } else {
         try {
             _fullyQualifiedDomainNameInCache = await promisify(fqdn)();
+            if (_fullyQualifiedDomainNameInCache === "localhost") {
+                throw new Error("localhost not expected");
+            }
             if (/sethostname/.test(_fullyQualifiedDomainNameInCache as string)) {
                 throw new Error("Detecting fqdn  on windows !!!");
             }
@@ -63,11 +72,18 @@ export function getFullyQualifiedDomainName(optional_max_length?: number) {
     return _fullyQualifiedDomainNameInCache ? trim(_fullyQualifiedDomainNameInCache, optional_max_length) : "%FQDN%";
 }
 
+export function getHostname() {
+    return os.hostname();
+}
+
 export function resolveFullyQualifiedDomainName(str: string): string {
     if (!_fullyQualifiedDomainNameInCache) {
         throw new Error("FullyQualifiedDomainName computation is not completed yet");
     }
-    return str.replace("%FQDN%", _fullyQualifiedDomainNameInCache);
+    str = str.replace("%FQDN%", _fullyQualifiedDomainNameInCache);
+    str = str.replace("{FQDN}", _fullyQualifiedDomainNameInCache);
+    str = str.replace("{hostname}", getHostname());
+    return str;
 }
 // note : under windows ... echo %COMPUTERNAME%.%USERDNSDOMAIN%
 prepareFQDN();
